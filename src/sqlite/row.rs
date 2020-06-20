b5a8dc0881b37a2389cc6947d47810e3eea371ba
kmgy <bhnk1270223@gn.iwasaki.ac.jp>
2020-06-21 00:50:09 +0900

    Added methods of Row     * column_count()     * column_names()     * column_name()

--- src/sqlite/row.rs
+++ src/sqlite/row.rs
@@ -18,6 +18,23 @@ impl Row {
     pub fn get(&self, key: &str) -> Option<&str> {
         self.value.get(key)?.as_deref()
     }
+
+    /// Return the number of columns.
+    pub fn column_count(&self) -> usize {
+        self.value.len()
+    }
+
+    /// Get all the column names.  
+    /// Column order is not guaranteed.
+    pub fn column_names(&self) -> Vec<&str> {
+        self.value.keys().map(|k| (*k).as_str()).collect::<Vec<_>>()
+    }
+
+    /// Return the name of the column.  
+    /// Column order is not guaranteed.
+    pub fn column_name(&self, col: usize) -> Option<&str> {
+        self.column_names().get(col).copied()
+    }
 }
 
 #[cfg(test)]
@@ -33,6 +50,11 @@ mod tests {
         assert_eq!(row.get("key1").unwrap(), "value");
         assert_eq!(row.get("key2"), None);
         assert_eq!(row.get("key3"), None);
+        assert_eq!(row.column_count(), 2);
+        assert!(row.column_names() == vec!["key1", "key2"] || row.column_names() == vec!["key2", "key1"]);
+        assert!(row.column_name(0) == Some("key1") || row.column_name(0) == Some("key2"));
+        assert!(row.column_name(1) == Some("key1") || row.column_name(1) == Some("key2"));
+        assert_eq!(row.column_name(2), None);
     }
 }
 
