40d9b0730421f30cc539382257a940991d215992
kmgy <bhnk1270223@gn.iwasaki.ac.jp>
2020-07-09 01:08:11 +0900

    Added tests for postgres

--- tests/postgres.rs
+++ tests/postgres.rs
@@ -2,6 +2,12 @@
 #[cfg(debug_assertions)]
 mod postgres {
     use owsql::params;
+    use owsql::error::*;
+
+    macro_rules! err {
+        () => { Err(owsql::error::OwsqlError::AnyError) };
+        ($msg:expr) => { Err(owsql::error::OwsqlError::Message($msg.to_string())) };
+    }
 
     fn stmt() -> &'static str {
         r#"CREATE TEMPORARY TABLE users (name TEXT, age INTEGER);
@@ -237,5 +243,205 @@ mod postgres {
         let sql = conn.ow("select age from users where age <") + &conn.allowlist(age) + &conn.ow(";");
 
         conn.iterate(&sql, |_| { unreachable!(); }).unwrap();
+    }
+
+    #[test]
+    fn int() {
+        let conn = owsql::postgres::open("postgresql://postgres:postgres@localhost").unwrap();
+        let stmt = conn.ow(stmt());
+        conn.execute(&stmt).unwrap();
+
+        let invalid = conn.int("invalid");
+
+        assert_ne!(&invalid, &conn.int(42));
+        assert_ne!(&invalid, &conn.int("42"));
+        assert_ne!(&invalid, &conn.int("42".to_string()));
+        assert_ne!(&invalid, &conn.int(&"42".to_string()));
+        assert_eq!(&invalid, &conn.int(std::f64::consts::PI));
+        assert_eq!(&invalid, &conn.int('A'));
+        assert_eq!(&invalid, &conn.int("str"));
+    }
+
+    #[test]
+    fn sanitizing() {
+        let conn = owsql::postgres::open("postgresql://postgres:postgres@localhost").unwrap();
+        let stmt = conn.ow(stmt());
+        conn.execute(&stmt).unwrap();
+
+        let name = r#"<script>alert("&1");</script>"#;
+        let sql = conn.ow("INSERT INTO users VALUES(") + name + &conn.ow(", 12345);");
+
+        conn.execute(&sql).unwrap();
+
+        conn.rows(conn.ow("SELECT name FROM users WHERE age = 12345;")).unwrap().iter() .all(|row| {
+            assert_eq!(row.get("name").unwrap(), "&lt;script&gt;alert(&quot;&amp;1&quot;);&lt;/script&gt;");
+            true
+        });
+        assert_eq!(
+            conn.actual_sql( unsafe { conn.ow_without_html_escape(&name) }),
+            Ok(format!("'{}' ", name))
+        );
+    }
+
+    #[test]
+    fn error_level() {
+        use owsql::error::OwsqlErrorLevel;
+
+        let mut conn = owsql::postgres::open("postgresql://postgres:postgres@localhost").unwrap();
+        conn.error_level(OwsqlErrorLevel::AlwaysOk).unwrap();
+        conn.error_level(OwsqlErrorLevel::Release).unwrap();
+        conn.error_level(OwsqlErrorLevel::Develop).unwrap();
+        conn.error_level(OwsqlErrorLevel::Debug).unwrap();
+    }
+
+    #[test]
+    #[allow(non_snake_case)]
+    fn error_level_AlwaysOk() {
+        let mut conn = owsql::postgres::open("postgresql://postgres:postgres@localhost").unwrap();
+        conn.error_level(OwsqlErrorLevel::AlwaysOk).unwrap();
+        let single_quote = conn.ow("'");
+        conn.add_allowlist(params!["Alice"]);
+        let name = conn.allowlist("Bob");
+        let integer = conn.int("50 or 1=1; --");
+
+        assert_eq!(conn.execute("INVALID SQL"),                     Ok(()));
+        assert_eq!(conn.execute("'endless"),                        Ok(()));
+        assert_eq!(conn.execute(&single_quote),                     Ok(()));
+        assert_eq!(conn.execute(&name),                             Ok(()));
+        assert_eq!(conn.execute(&integer),                          Ok(()));
+        assert_eq!(conn.iterate("INVALID SQL", |_| unreachable!()), Ok(()));
+        assert_eq!(conn.iterate("'endless",    |_| unreachable!()), Ok(()));
+        assert_eq!(conn.iterate(&single_quote, |_| unreachable!()), Ok(()));
+        assert_eq!(conn.iterate(&name,         |_| unreachable!()), Ok(()));
+        assert_eq!(conn.iterate(&integer,      |_| unreachable!()), Ok(()));
+        assert_eq!(conn.rows("INVALID SQL"),                        Ok(vec![]));
+        assert_eq!(conn.rows("'endless"),                           Ok(vec![]));
+        assert_eq!(conn.rows(&single_quote),                        Ok(vec![]));
+        assert_eq!(conn.rows(&name),                                Ok(vec![]));
+        assert_eq!(conn.rows(&integer),                             Ok(vec![]));
+    }
+
+    #[test]
+    fn error_level_release() {
+        let mut conn = owsql::postgres::open("postgresql://postgres:postgres@localhost").unwrap();
+        conn.error_level(OwsqlErrorLevel::Release).unwrap();
+        let single_quote = conn.ow("'");
+        conn.add_allowlist(params!["Alice"]);
+        let name = conn.allowlist("Bob");
+        let integer = conn.int("50 or 1=1; --");
+
+        assert_eq!(conn.execute("INVALID SQL"),                     err!());
+        assert_eq!(conn.execute("'endless"),                        err!());
+        assert_eq!(conn.execute(&single_quote),                     err!());
+        assert_eq!(conn.execute(&name),                             err!());
+        assert_eq!(conn.execute(&integer),                          err!());
+        assert_eq!(conn.iterate("INVALID SQL", |_| unreachable!()), err!());
+        assert_eq!(conn.iterate("'endless",    |_| unreachable!()), err!());
+        assert_eq!(conn.iterate(&single_quote, |_| unreachable!()), err!());
+        assert_eq!(conn.iterate(&name,         |_| unreachable!()), err!());
+        assert_eq!(conn.iterate(&integer,      |_| unreachable!()), err!());
+        assert_eq!(conn.rows("INVALID SQL"),                        err!());
+        assert_eq!(conn.rows("'endless"),                           err!());
+        assert_eq!(conn.rows(&single_quote),                        err!());
+        assert_eq!(conn.rows(&name),                                err!());
+        assert_eq!(conn.rows(&integer),                             err!());
+    }
+
+    #[test]
+    fn error_level_develop() {
+        let mut conn = owsql::postgres::open("postgresql://postgres:postgres@localhost").unwrap();
+        conn.error_level(OwsqlErrorLevel::Develop).unwrap();
+        let single_quote = conn.ow("'");
+        conn.add_allowlist(params!["Alice"]);
+        let name = conn.allowlist("Bob");
+        let integer = conn.int("50 or 1=1; --");
+
+        assert_eq!(conn.execute("INVALID SQL"),                     err!("exec error"));
+        assert_eq!(conn.execute("'endless"),                        err!("exec error"));
+        assert_eq!(conn.execute(&single_quote),                     err!("invalid literal"));
+        assert_eq!(conn.execute(&name),                             err!("deny value"));
+        assert_eq!(conn.execute(&integer),                          err!("non integer"));
+        assert_eq!(conn.iterate("INVALID SQL", |_| unreachable!()), err!("exec error"));
+        assert_eq!(conn.iterate("'endless",    |_| unreachable!()), err!("exec error"));
+        assert_eq!(conn.iterate(&single_quote, |_| unreachable!()), err!("invalid literal"));
+        assert_eq!(conn.iterate(&name,         |_| unreachable!()), err!("deny value"));
+        assert_eq!(conn.iterate(&integer,      |_| unreachable!()), err!("non integer"));
+        assert_eq!(conn.rows("INVALID SQL"),                        err!("exec error"));
+        assert_eq!(conn.rows("'endless"),                           err!("exec error"));
+        assert_eq!(conn.rows(&single_quote),                        err!("invalid literal"));
+        assert_eq!(conn.rows(&name),                                err!("deny value"));
+        assert_eq!(conn.rows(&integer),                             err!("non integer"));
+    }
+
+    #[test]
+    fn error_level_debug() {
+        let mut conn = owsql::postgres::open("postgresql://postgres:postgres@localhost").unwrap();
+        conn.error_level(OwsqlErrorLevel::Debug).unwrap();
+        let single_quote = conn.ow("'");
+        conn.add_allowlist(params!["Alice"]);
+        let name = conn.allowlist("Bob");
+        let integer = conn.int("50 or 1=1; --");
+
+        assert_eq!(conn.execute("INVALID SQL"),
+            err!("exec error: db error: ERROR: \"\'INVALID SQL\'\"またはその近辺で構文エラー"));
+        assert_eq!(conn.execute("'endless"),
+            err!("exec error: db error: ERROR: \"\'&#39;endless\'\"またはその近辺で構文エラー"));
+        assert_eq!(conn.execute(&single_quote),                     err!("invalid literal: '"));
+        assert_eq!(conn.execute(&name),                             err!("deny value: Bob"));
+        assert_eq!(conn.execute(&integer),                          err!("non integer: 50 or 1=1; --"));
+        assert_eq!(conn.iterate("INVALID SQL", |_| unreachable!()),
+            err!("exec error: db error: ERROR: \"\'INVALID SQL\'\"またはその近辺で構文エラー"));
+        assert_eq!(conn.iterate("'endless",    |_| unreachable!()),
+            err!("exec error: db error: ERROR: \"\'&#39;endless\'\"またはその近辺で構文エラー"));
+        assert_eq!(conn.iterate(&single_quote, |_| unreachable!()), err!("invalid literal: '"));
+        assert_eq!(conn.iterate(&name,         |_| unreachable!()), err!("deny value: Bob"));
+        assert_eq!(conn.iterate(&integer,      |_| unreachable!()), err!("non integer: 50 or 1=1; --"));
+        assert_eq!(conn.rows("INVALID SQL"),
+            err!("exec error: db error: ERROR: \"\'INVALID SQL\'\"またはその近辺で構文エラー"));
+        assert_eq!(conn.rows("'endless"),
+            err!("exec error: db error: ERROR: \"\'&#39;endless\'\"またはその近辺で構文エラー"));
+        assert_eq!(conn.rows(&single_quote),                        err!("invalid literal: '"));
+        assert_eq!(conn.rows(&name),                                err!("deny value: Bob"));
+        assert_eq!(conn.rows(&integer),                             err!("non integer: 50 or 1=1; --"));
+    }
+
+    #[test]
+    fn integer() {
+        let conn = owsql::postgres::open("postgresql://postgres:postgres@localhost").unwrap();
+        let stmt = conn.ow(stmt());
+        conn.execute(&stmt).unwrap();
+
+        let age = 50;
+        let sql = conn.ow("select name from users where age <") + &conn.int(age);
+
+        for row in conn.rows(&sql).unwrap().iter() {
+            assert_eq!(row.get("name").unwrap(), "Alice");
+        }
+    }
+
+    #[test]
+    fn ow_into_execute() {
+        let conn = owsql::postgres::open("postgresql://postgres:postgres@localhost").unwrap();
+        conn.execute(conn.ow("SELECT") + &conn.int(1)).unwrap();
+    }
+
+    #[test]
+    fn ow_into_iterate() {
+        let conn = owsql::postgres::open("postgresql://postgres:postgres@localhost").unwrap();
+        conn.iterate(conn.ow("SELECT") + &conn.int(1), |_| true ).unwrap();
+    }
+
+    #[test]
+    fn ow_into_rows() {
+        let conn = owsql::postgres::open("postgresql://postgres:postgres@localhost").unwrap();
+        for row in conn.rows(conn.ow("SELECT") + &conn.int(1)).unwrap().iter() {
+            assert_eq!(row.get("?column?").unwrap(), "1");
+        }
+    }
+
+    #[test]
+    fn empty_string() {
+        let conn = owsql::postgres::open("postgresql://postgres:postgres@localhost").unwrap();
+        assert_eq!(conn.actual_sql(""), Ok("".to_string()));
     }
 }
