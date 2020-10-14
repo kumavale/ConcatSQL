87786983174e9d108b248096dc4196f1753ed502
kmgy <bhnk1270223@gn.iwasaki.ac.jp>
2020-10-14 14:39:47 +0900

    Oops... I forgot to change the name...

--- tests/postgres.rs
+++ tests/postgres.rs
@@ -236,7 +236,7 @@ mod postgres {
 
         conn.rows(conn.ow("SELECT name FROM users WHERE age = 12345;")).unwrap().iter() .all(|row| {
             assert_eq!(
-                owsql::escape_html(row.get("name").unwrap()),
+                owsql::html_special_chars(row.get("name").unwrap()),
                 "&lt;script&gt;alert(&quot;&amp;1&quot;);&lt;/script&gt;"
             );
             true
