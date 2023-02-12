081a94d5f943db5316b77fa857e8b329dd266807
kumavale <kumavale.hyperion@gmail.com>
2023-02-12 20:11:02 +0900

    add tests

--- concatsql_macro/tests/macro.rs
+++ concatsql_macro/tests/macro.rs
@@ -16,4 +16,21 @@ mod macros {
         let sql = query!(r#"{{name}}"#);
         assert_eq!(sql.simulate(), "{name}");
     }
+
+    #[test]
+    fn query_value_test() {
+        let i32_: i32 = 1;
+        let i64_: i64 = 2;
+        let f32_: f32 = 3.;
+        let f64_: f64 = 4.;
+        let text: String = "5".to_string();
+
+        let sql = query!(r#"{i32_}{i64_}{f32_}{f64_}{text}"#);
+        assert_eq!(sql.simulate(), "1234'5'");
+    }
+
+    //#[test]
+    //fn query_compile_error_not_found_test() {
+    //    _ = query!(r#"{var}"#);
+    //}
 }
